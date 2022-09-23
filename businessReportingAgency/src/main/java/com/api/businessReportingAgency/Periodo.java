4e08076ad9ad5d1e8f45ebb23de2dbe82d4a1b55
Karen Almeida <karenalmeida61@hotmail.com>
2022-09-23 13:43:38 -0300

    adicionando entidade report

---
+++ businessReportingAgency/src/main/java/com/api/businessReportingAgency/Periodo.java
@@ -0,0 +1,5 @@
+package com.api.businessReportingAgency;
+
+public enum Periodo {
+    ANUAL, SEMESTRAL, MENSAL
+}
---
+++ businessReportingAgency/src/main/java/com/api/businessReportingAgency/Report.java
@@ -0,0 +1,26 @@
+package com.api.businessReportingAgency;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+public class Report {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String cnpj;
+    private String descricao;
+    private Periodo periodo;
+}
