2c3626a36726fbfe846da0848f91c825046ecb90
Karen Almeida <karenalmeida61@hotmail.com>
2022-09-23 19:15:48 -0300

    Adicionar entidade employee

--- businessReportingAgency/src/main/java/com/api/businessReportingAgency/repositories/ReportRepository.java
+++ businessReportingAgency/src/main/java/com/api/businessReportingAgency/repositories/ReportRepository.java
@@ -3,7 +3,6 @@ package com.api.businessReportingAgency.repositories;
 import com.api.businessReportingAgency.models.Report;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
 @Repository
