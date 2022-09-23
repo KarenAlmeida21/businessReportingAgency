8592dd18c0ee3bd0e99f04a691177a67b9a0ac9e
Karen Almeida <karenalmeida61@hotmail.com>
2022-09-23 13:33:45 -0300

    first commit

---
+++ businessReportingAgency/src/main/java/com/api/businessReportingAgency/BusinessReportingAgencyApplication.java
@@ -0,0 +1,13 @@
+package com.api.businessReportingAgency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessReportingAgencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessReportingAgencyApplication.class, args);
+	}
+
+}
---
+++ businessReportingAgency/src/test/java/com/api/businessReportingAgency/BusinessReportingAgencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.businessReportingAgency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessReportingAgencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
